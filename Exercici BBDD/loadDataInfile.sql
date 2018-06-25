ef58e493307f51019c00ec2acdfe23a7b55549a2
Marc Portabella <portabella.marc@gmail.com>
2018-06-25 13:29:36 +0200

    Exercici 2 SQL fet

---
+++ Exercici BBDD/loadDataInfile.sql
@@ -0,0 +1,44 @@
+use USAirlineFlights;
+/*TRUNCATE TABLE flights;
+
+/*SHOW VARIABLES LIKE 'secure_file_priv';*/
+
+LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 5.7/Uploads/USFlights/data/carriers.csv'
+INTO TABLE carriers 
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 5.7/Uploads/USFlights/data/airports.csv'
+INTO TABLE usairports 
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 5.7/Uploads/USFlights/data/2000.csv'
+INTO TABLE flights 
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(colYear, colMonth, DayOfMonths, DayOfWeek, @DepTime, CRSDepTime, @ArrTime, CRSArrTime, UniqueCarrier, FlightNum, TailNum, @ActualElapsedTime, CRSElapsedTime, @AirTime, @ArrDelay, @DepDelay, Origin, Dest, Distance, TaxiIn, TaxiOut, Cancelled, CancellationCode, Diverted)
+SET
+  DepTime = CASE WHEN @DepTime NOT IN ('NA', 'NaN', '--') THEN @DepTime END,
+  ArrTime = CASE WHEN @ArrTime NOT IN ('NA', 'NaN', '--') THEN @ArrTime END,
+  ActualElapsedTime = CASE WHEN @ActualElapsedTime NOT IN ('NA', 'NaN', '--') THEN @ActualElapsedTime END,
+  ArrDelay = CASE WHEN @ArrDelay NOT IN ('NA', 'NaN', '--') THEN @ArrDelay END,
+  DepDelay = CASE WHEN @DepDelay NOT IN ('NA', 'NaN', '--') THEN @DepDelay END;
+  
+  
+  LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 5.7/Uploads/USFlights/data/2000x2000.csv'
+INTO TABLE flights 
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(colYear, colMonth, DayOfMonths, DayOfWeek, @DepTime, CRSDepTime, @ArrTime, CRSArrTime, UniqueCarrier, FlightNum, TailNum, @ActualElapsedTime, CRSElapsedTime, @AirTime, @ArrDelay, @DepDelay, Origin, Dest, Distance, TaxiIn, TaxiOut, Cancelled, CancellationCode, Diverted)
+SET
+  DepTime = CASE WHEN @DepTime NOT IN ('NA', 'NaN', '--') THEN @DepTime END,
+  ArrTime = CASE WHEN @ArrTime NOT IN ('NA', 'NaN', '--') THEN @ArrTime END,
+  ActualElapsedTime = CASE WHEN @ActualElapsedTime NOT IN ('NA', 'NaN', '--') THEN @ActualElapsedTime END,
+  ArrDelay = CASE WHEN @ArrDelay NOT IN ('NA', 'NaN', '--') THEN @ArrDelay END,
+  DepDelay = CASE WHEN @DepDelay NOT IN ('NA', 'NaN', '--') THEN @DepDelay END;
+  
---
+++ Exercici BBDD/queries.sql
@@ -0,0 +1,39 @@
+/*Query 1*/
+/*SELECT COUNT(*) FROM flights;*/
+
+/*Query 2*/
+SELECT Origin, AVG(DepDelay), AVG(ArrDelay)
+FROM usairlineflights.flights 
+GROUP BY Origin;
+
+/*Query 3*/
+SELECT Origin, colYear, colMonth, AVG(ArrDelay)
+FROM usairlineflights.flights 
+GROUP BY Origin, colMonth;
+
+/*Query 4*/
+SELECT usairports.City, colYear, colMonth, AVG(ArrDelay)
+FROM usairlineflights.flights
+INNER JOIN usairports ON flights.Origin=usairports.IATA
+GROUP BY Origin, colMonth;
+
+/*Query 5*/
+SELECT carriers.Description, SUM(Cancelled)
+FROM usairlineflights.flights
+INNER JOIN carriers ON flights.UniqueCarrier=carriers.CarrierCode
+GROUP BY UniqueCarrier
+ORDER BY SUM(Cancelled) DESC;
+
+/*Query 6*/
+SELECT TailNum, SUM(Distance) AS total_distance
+FROM usairlineflights.flights
+WHERE Cancelled = 0
+GROUP BY TailNum
+ORDER BY total_distance DESC;
+
+/*Query 7*/
+SELECT carriers.Description, AVG(ArrDelay) AS AverageDelay
+FROM usairlineflights.flights
+INNER JOIN carriers ON flights.UniqueCarrier=carriers.CarrierCode
+GROUP BY UniqueCarrier
+HAVING (AverageDelay) > 10
